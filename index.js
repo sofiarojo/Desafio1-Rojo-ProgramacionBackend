ac1560b41a21efbe92a98dae703eee7a3c2e8b3e
Sofia Rojo <sofiarojo27@gmail.com>
2023-08-27 23:06:12 -0300

    Avances

--- index.js
+++ index.js
@@ -29,4 +29,29 @@ class ProductManager {
 
         this.#products.push(product);
     }
-}
+
+    getProducts() {
+        return this.#products;
+    }
+
+    getProductById(id) {
+        const product = this.#products.find((product) => product.id === id );
+
+        if (!product) {
+            console.error("Producto no encontrado.");
+        }
+
+        return product;
+    }
+};
+
+
+//Ejemplo de uso:
+const productManager = new ProductManager();
+
+productManager.addProduct("Producto prueba 1", "Descripcion 1", 10, "img1.jpg", "abc123", 50);
+productManager.addProduct("Producto prueba 2", "Descripcion 2", 15, "img2.jpg", "def456", 30);
+
+console.log(productManager.getProducts());
+console.log(productManager.getProductById(1));
+console.log(productManager.getProductById(3));  //Acá mostrará un error
